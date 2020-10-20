d3ad1eee1ad2c8ca76a854abbc88e558494b8b4b
Divyashree-V <divyasuvarna.v@gmail.com>
2020-10-20 16:07:25 +0530

    count the visitors in working hours (9-5)

--- Lab6/index.php
+++ Lab6/index.php
@@ -1,6 +1,11 @@
 <html>
 <head>
     <title>program 6</title>
+    <style>
+        h1,h2{
+            text-align: center;
+        }
+    </style>
 </head>
 <body>
     <h1>Welcome to my webpage </h1>
@@ -11,8 +16,13 @@
         fclose($handle);
         $count=intval($count);
         $count += 1;
-        echo "<h2>No of visitors who visited this page : $count </h2>";
+        //Visitors working time
+        date_default_timezone_set('Asia/Kolkata');
+        $cur_time= date("H");
+        if ($cur_time > 9 && $cur_time < 17)
+            $count += 1;
 
+        echo "<h2>No of visitors who visited this page : $count </h2>";
         $handle=fopen("count.txt","w");
         fwrite($handle, $count);
         fclose($handle);
